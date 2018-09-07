952ae33aaa59aa862e595ea7ec2ab2379bd645d2
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-09-07 11:47:59 +0100

    Start handleNewCharacter, console test passed

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('listener working');
-})
+
+  const newCharacter = document.querySelector('#starWarsForm');
+  newCharacter.addEventListener('submit', handleNewCharacter);
+});
+
+const handleNewCharacter = function (event) {
+  event.preventDefault();
+  console.log('button pressed');
+};
