d833d0ff5fe010c585d2da070c991e38b8582dfe
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-09-07 15:13:49 +0100

    Add new character function

--- js/main.js
+++ js/main.js
@@ -1,11 +1,49 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('listener working');
 
-  const newCharacter = document.querySelector('#starWarsForm');
+  const newCharacter = document.querySelector('#star-wars-form');
   newCharacter.addEventListener('submit', handleNewCharacter);
 });
 
 const handleNewCharacter = function (event) {
   event.preventDefault();
-  console.log('button pressed');
+  //create character
+  const character = createNewCharacter(event.target);
+  console.log(event.target);
+
+  //select element to insert
+  list = document.querySelector('#output')
+
+  //append to element
+  list.appendChild(character)
+
+  event.target.reset();
+};
+
+const createNewCharacter = function (form) {
+
+  //character
+  const newCharacterContainer = document.createElement('tr')
+
+
+  // create name
+  const name = document.createElement('th');
+  name.textContent = form.name.value;
+
+  //create class
+
+  const characterClass = document.createElement('th');
+  characterClass.textContent = form.class.value;
+
+  //create forceSensitive
+
+  const force = document.createElement('th');
+  force.textContent = form.forceSensitive.value;
+
+  //append to container
+  newCharacterContainer.appendChild(name);
+  newCharacterContainer.appendChild(characterClass);
+  newCharacterContainer.appendChild(force);
+
+  return newCharacterContainer
+
 };
